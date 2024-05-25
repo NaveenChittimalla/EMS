049da16d7b5864b9a44e0abd7fc93c3979eeaf96
NaveenChittimalla <naveenjits431@gmail.com>
2024-05-25 11:16:14 +0000

    chnages merged

--- Database.sql
+++ Database.sql
@@ -12,13 +12,93 @@ CREATE TABLE [dbo].[Employee] (
 );
 GO
 
-CREATE PROCEDURE dbo.Employee_Update_Details (
+CREATE FUNCTION [dbo].[FN_Employee_Generate_Code]
+(
+	@EmployeeId int
+)
+RETURNS NVARCHAR(100)
+AS
+BEGIN
+	RETURN CONCAT('EMS', @EmployeeId)
+END
+GO
+
+CREATE PROCEDURE [dbo].[Employee_Insert]
+    @FirstName NVARCHAR(250),
+    @LastName NVARCHAR(250),
+    @Email NVARCHAR(250),
+    @Active BIT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    INSERT INTO Employee
+    (
+        FirstName,
+        LastName,
+        Email,
+        Active
+    )
+    values
+    (
+        @FirstName,
+        @LastName,
+        @Email,
+        @Active
+    );
+
+    DECLARE @NewEmployeeId INT
+    SET @NewEmployeeId = @@IDENTITY
+    
+    UPDATE Employee
+    SET EmployeeCode = [dbo].[FN_Employee_Generate_Code](@NewEmployeeId)
+    FROM dbo.Employee Employee
+    WHERE id = @NewEmployeeId;
+    
+    SELECT @NewEmployeeId
+END
+GO
+
+CREATE PROCEDURE [dbo].[Employee_GetAll]
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+	SELECT 
+		Id,
+		EmployeeCode,
+		FirstName,
+		LastName,
+		Email,
+		Active
+	FROM dbo.Employee
+END
+GO
+
+CREATE PROCEDURE [dbo].[Employee_GetById]
+	@Id INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+	SELECT 
+		Id,
+		EmployeeCode,
+		FirstName,
+		LastName,
+		Email,
+		Active
+	FROM dbo.Employee
+	WHERE Id = @Id
+END
+GO
+
+CREATE PROCEDURE [dbo].[Employee_Update_Details] 
     @Id INT,
     @FirstName NVARCHAR(250),
     @LastName NVARCHAR(250),
     @Email NVARCHAR(550),
     @Active BIT
-)
 AS
 BEGIN
   SET NOCOUNT ON;
@@ -31,5 +111,15 @@ BEGIN
   FROM dbo.Employee Employee
   WHERE Id = @Id
 
+END
+GO
+
+CREATE PROCEDURE [dbo].[Employee_DeleteById] @Id INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DELETE FROM Employee
+    WHERE Id = @Id
 END
 GO
